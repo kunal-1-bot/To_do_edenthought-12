52666e572c0c11218f46753f098b5325e66d6315
kunal kumar <139539262+kunal-1-bot@users.noreply.github.com>
2025-12-27 18:03:49 +0530

    minor chnge

--- edenthought/edenthought/settings.py
+++ edenthought/edenthought/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -150,6 +150,6 @@ EMAIL_PORT ='587'
 EMAIL_USE_TLS ='True'
 
 EMAIL_HOST_USER = 'creatorthoughtsss@gmail.com'
-EMAIL_HOST_PASSWORD ='wgvjwkdauwsxuash'
+EMAIL_HOST_PASSWORD ='passss'
 
 DEFAULT_FROM_EMAIL = 'creatorthoughtsss@gmail.com'
